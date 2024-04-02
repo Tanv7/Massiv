2555e5fcd38cb335a8d6e6f0e05d76c317188f8b
Tanv7 <tanv7@yandex.ru>
2024-04-02 11:18:33 +0300

    Массивы

--- src/Main.java
+++ src/Main.java
@@ -63,3 +63,58 @@ public class Main {
         for (int i=0;i< m.length;i++){
 
         }
+        public static int indexOfNearestByModul( int x,int []m){
+            int minDif = x - m[0];
+            int j = 0;
+            for (int i = 1; i < m.length; i++) {
+                if ((x - m[i]) < minDif) minDif = x - m[i];
+                j = 1;
+            }
+            return j;
+        }
+        public static void printBackward(int[]m){
+            for (int i = m.length - 1; i >= 0; i--) {
+                System.out.print(m[i] + "");
+            }
+            System.out.println("");
+        }
+        public static void printFromTO(int i,int j,int[]m){
+            for (;i<=j;i++)
+        }
+        public static int indexOf(int x,int[]m){
+            return-1;
+        }
+        public static int indexOfNearestByModule (int x,int[]m){
+            int minDif= x-m[0];
+            int j=0;
+            for( int i=1;i<minDif) minDif=x-m[i];
+            j=1;
+        }
+        return j;
+        }
+        public static void printBackward(int[]m){
+        for( int i=m.length-1;i>0;i--){
+            System.out.printLn("");
+        }
+        System.out.println("");
+        }
+        public static void printFromTo(int i,int j,int[]m){
+        for (;i<=j;i++)
+        }
+        public class Main{
+        public static int findElNomNearValue(int[]) m,int val){
+            }
+            public static void printReverseOrderint (int[]m){
+System.out.printLn( "Элементы массива в обратном порядке:")
+for ( int i=(m.length-1);i>=0;i--)
+System.out.print(m[i]+";");
+System.out.println();
+    }
+public static void printElBetweenXY(int[] ,int x,int y){
+}
+System.out.println( "Элементы массива между "+x+" и "+y);
+for (int i=0;i<m.length;i++)
+if(m[i]>=x && m[i]<=y)
+System.out.println();
+}
+}
